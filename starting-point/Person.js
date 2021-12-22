8b94887c4092ca094e4540548094d1c0226e505f
only1finesse <stanomotuyole@gmail.com>
2021-12-22 10:39:39 -0800

    adds jest tests for Person class

--- starting-point/Person.js
+++ starting-point/Person.js
@@ -1,8 +1,47 @@
 class Person {
-	constructor(name){
-		this.name = name
-		this.bag = []
+	constructor(name, flightNo, role){
+		this.name = name;
+		this.flightNo = flightNo
+		this.role = role;
+	}
+
+	checkIn() {
+		return [this.role, this.name]
 	}
 }
 
-module.exports = Person
+class Passenger extends Person {
+	constructor(name, flightNo) {
+		super(name, flightNo); 
+		this.role = 'Passenger'
+	}
+
+}
+
+class Crew extends Person {
+	constructor(name, flightNo) {
+		super(name, flightNo); 
+		this.role = 'Crew'
+	}
+
+	greet() {
+		return `Welcome to flightNo ${this.flightNo}. Fasten your seatbelts!`
+	}
+
+}
+
+class Pilot extends Person {
+	constructor(name, flightNo) {
+		super(name, flightNo); 
+		this.role = 'Pilot'
+	}
+
+	fly() {
+		return `Flight ${this.flightNo} ready for take off. Have a nice flightNo!`
+	}
+
+}
+
+
+
+module.exports = {Person, Passenger, Crew, Pilot}
